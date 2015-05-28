4d80e56f27f53edffcc974358ed1404634540078
James <j.fallon1997@gmail.com>
2015-05-28 21:16:45 +0100

    Update chooser.sh
    Changed some strings to variables for future compatibility

--- chooser.sh
+++ chooser.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Created May 2015. Last update 2015-05-28 j.fallon@linux.com
+CONFDIR='/home/'$USER'/bin/screen-conf' # What directory are additional configuration files stored in?
+MYSHELL=$SHELL # Allow user to specify their shell
+####################################################
 echo -e "\E[0;30m""GNU Screen Session Chooser" #Greeter
 tput sgr0 # Reset prompt text color
 
@@ -16,23 +19,23 @@ do
         	screen -R # if possible, reconnect to an existing session
             ;;
         "No Screen")
-		$SHELL # start the user's shell
+		$MYSHELL # start the user's shell
             ;;
 
 	# Custom start files. 
 
 ######### I recommend removing these options and replacing them with more suitbale ones for your own system. I have left them here as an example
 #        "TeX Development Environment")
-#		screen -c /home/$USER/bin/screen-conf/TeX 
+#		screen -c $CONFDIR'/TeX' 
 #            ;;
 #        "Miscellaneous Development Environment")
-#		screen -c /home/$USER/bin/screen-conf/misc
+#		screen -c $CONFDIR'/misc'
 #            ;;
 #        "SSH split with pi@192.168.1.4")
-#		screen -c /home/$USER/bin/screen-conf/ssh-pi
+#		screen -c $CONFDIR'/ssh-pi'
 #            ;;
 #	"pacaur -Syu")
-#		screen -c /home/$USER/bin/screen-conf/screen-pacsyu
+#		screen -c $CONFDIR'/screen-pacsyu'
 #	    ;;
 ########
 	# Close the terminal
